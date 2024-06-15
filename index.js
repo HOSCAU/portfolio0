3b16cd527da72f73bd0cb7ea4d70575b70294b3c
Aditya R S <139953936+HOSCAU@users.noreply.github.com>
2024-06-15 08:21:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import About1 from "./About1.jpg";
+import project1 from "./Project1.png";
+import project2 from "./project2.png";
+import contactImg from "./contactImg.png";
+import Aditya12 from "./Aditya12.jpeg";
+
+export {
+    About1,
+    project1,
+    project2,
+    contactImg,
+    Aditya12,
+}
